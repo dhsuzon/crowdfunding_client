dffe53473891e80cfa6c4cebf6761fcbf591aa7a
Dider Hossain Suzon <diderhossainsuzon@gmail.com>
2026-07-15 20:00:00 +0600

    chore: update Next.js config and add new dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: '**' },
+    ],
+  },
 };
 
 export default nextConfig;
